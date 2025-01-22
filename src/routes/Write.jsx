ddb1285405dbec5345083ca6e3c86623ede3fd4d
dhanbyte <dhananjay.win2004@gmail.com>
2025-01-22 21:16:32 +0530

    all set

--- src/routes/Write.jsx
+++ src/routes/Write.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import ReactQuill from 'react-quill';
+import ReactQuill from "react-quill";
+// import ReactQuill from 'react-quill';
 
 export default function Write() {
   return (
